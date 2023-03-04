59cb0cde2c1c72bb5009da797a8052e57823bec6
Derek Pierson <drk.pierson@gmail.com>
2023-03-03 20:35:53 -0700

    Functional!

---
+++ CN_RXEQ.R
@@ -0,0 +1,110 @@
+# MIMICS-CN Rev-MM Eq.
+
+CN_RXEQ <- function(t, y, pars) {
+  with (as.list(c(y, pars)),{
+    
+    LITmin[1] = MIC_1 * VMAX[1] * LIT_1 / (KM[1] + MIC_1)
+    LITmin[2] = MIC_1 * VMAX[2] * LIT_2 / (KM[2] + MIC_1)
+    MICtrn[1] = MIC_1^densDep * tau[1] * fPHYS[1] 
+    MICtrn[2] = MIC_1^densDep * tau[1] * fCHEM[1] 
+    MICtrn[3] = MIC_1^densDep * tau[1] * fAVAI[1] 
+    SOMmin[1] = MIC_1 * VMAX[3] * SOM_3 / (KM[3] + MIC_1)
+    
+    LITmin[3] = MIC_2 * VMAX[4] * LIT_1 / (KM[4] + MIC_2) 
+    LITmin[4] = MIC_2 * VMAX[5] * LIT_2 / (KM[5] + MIC_2)
+    MICtrn[4] = MIC_2^densDep * tau[2] * fPHYS[2]
+    MICtrn[5] = MIC_2^densDep * tau[2] * fCHEM[2] 
+    MICtrn[6] = MIC_2^densDep * tau[2] * fAVAI[2]     
+    SOMmin[2] = MIC_2 * VMAX[6] * SOM_3 / (KM[6] + MIC_2)
+    
+    DEsorb    = SOM_1 * desorb
+    
+    OXIDAT    = ((MIC_1 * VMAX[2] * SOM_2 / (KO[1]*KM[2] + MIC_1)) +
+                   (MIC_2 * VMAX[5] * SOM_2 / (KO[2]*KM[5] + MIC_2)))
+    
+    upMIC_1    = CUE[1]*(LITmin[1] + SOMmin[1]) + CUE[2]*(LITmin[2])
+    upMIC_2    = CUE[3]*(LITmin[3] + SOMmin[2]) + CUE[4]*(LITmin[4])   
+    
+    dLIT_1 = Inputs[1]*(1-FI[1]) - LITmin[1] - LITmin[3]
+    dMIC_1 = upMIC_1 - (MICtrn[[1]] + MICtrn[[2]] + MICtrn[[3]])# - Overflow[1]    
+    dSOM_1 = Inputs[1]*FI[1] + MICtrn[1] + MICtrn[4] - DEsorb     
+    
+    dLIT_2 = Inputs[2]*(1-FI[2]) - LITmin[2] - LITmin[4]
+    dMIC_2 = upMIC_2 - (MICtrn[[4]] + MICtrn[[5]] + MICtrn[[6]])# - Overflow[2]    
+    dSOM_2 = Inputs[2]*FI[2] + MICtrn[2] + MICtrn[5] - OXIDAT
+    
+    dSOM_3 = MICtrn[3] + MICtrn[6] + DEsorb + OXIDAT - SOMmin[1] - SOMmin[2]
+    
+    #------ N cycle ---------
+    
+    MICr_recip = 1.0 / (MIC_1 + 1e-10)
+    LITminN[1] =  LITmin[1]*LIT_1_N/(LIT_1 + 1e-10)
+    LITminN[2] =  LITmin[2]*LIT_2_N/(LIT_2 + 1e-10)
+    MICtrnN[1] =  MICtrn[1]*MIC_1_N * MICr_recip
+    MICtrnN[2] =  MICtrn[2]*MIC_1_N * MICr_recip
+    MICtrnN[3] =  MICtrn[3]*MIC_1_N * MICr_recip
+    SOMminN[1] =  SOMmin[1]*(SOM_3_N/(SOM_3 + 1e-10))
+  
+    MICk_recip = 1.0 / (MIC_2 + 1.0e-10)
+    LITminN[3] =  LITmin[3]*LIT_1_N/(LIT_1 + 1e-10)
+    LITminN[4] =  LITmin[4]*LIT_2_N / (LIT_2 + 1e-10)
+    MICtrnN[4] =  MICtrn[4] * MIC_2_N * MICk_recip
+    MICtrnN[5] =  MICtrn[5] * MIC_2_N * MICk_recip
+    MICtrnN[6] =  MICtrn[6] * MIC_2_N * MICk_recip
+    SOMminN[2] =  SOMmin[2]*(SOM_3_N/(SOM_3 + 1e-10))
+  
+    DEsorbN    =  DEsorb[1]*(SOM_1_N/(SOM_1 + 1e-10))
+  
+    OXIDATN    =  OXIDAT[1] * (SOM_2_N/(SOM_2 + 1e-10))
+
+  #! Partitions available DIN between microbial pools based on relative biomass
+  #! The amount of DIN uptake is not demand driven.  It is a fraction of a large pool. -mdh 7/29/2019
+
+    #! Assume that microbes take up all mineral N that is available. The unneeded portion will be spilled later.
+    DINup[1]   = DIN*MIC_1/(MIC_1+MIC_2+ 1e-10) #!!! Emily's code first multiplies by (1-Nleak)
+    DINup[2]   = DIN*MIC_2/(MIC_1+MIC_2+ 1e-10) #!!! Emily's code first multiplies by (1-Nleak)
+  
+    upMIC_1_N  = NUE[1]*(LITminN[1] + SOMminN[1]) + NUE[2]*(LITminN[2]) + DINup[1] #!!! Emily's code only has one NUE value
+  
+    CNup[1]    = (upMIC_1)/(upMIC_1_N + 1e-10)
+  #   Overflow[1] = (upMIC_1) - (upMIC_1_N)*min(CN_r, CNup[1])
+    Nspill[1]   = (upMIC_1_N) - (upMIC_1)/max(CN_r, CNup[1])
+  
+  # #! Add Overflow_r and Overflow_k to output netCDF file. Units conversion occurs in subroutine mimics_caccum. -mdh 10/12/2020
+  # #mimicsflux%Overflow_r(npt) = mimicsflux%Overflow_r(npt) + Overflow_r
+  
+    upMIC_2    = CUE[3]*(LITmin[3] + SOMmin[2]) + CUE[4]*(LITmin[4])
+    upMIC_2_N  = NUE[3]*(LITminN[3] + SOMminN[2]) + NUE[4]*(LITminN[4]) + DINup[2]
+  
+    CNup[2]    = (upMIC_2)/(upMIC_2_N+1e-10)
+  #   Overflow[2] = (upMIC_2) - (upMIC_2_N)*min(CN_K, CNup[2])
+    Nspill[2]   = (upMIC_2_N) - (upMIC_2)/max(CN_K, CNup[2])
+  
+  # #! Add Overflow_r and Overflow_k to output netCDF file. Units conversion occurs in subroutine mimics_caccum. -mdh 10/12/2020
+  # #mimicsflux%Overflow_k(npt) = mimicsflux%Overflow_k(npt) + Overflow_k
+  
+    dLIT_1_N = Inputs[1]*(1-FI[1])/CN_m - LITminN[1] - LITminN[3]
+    dMIC_1_N = upMIC_1_N - (MICtrnN[[1]] + MICtrnN[[2]] + MICtrnN[[3]]) - Nspill[1]
+    dSOM_1_N = Inputs[1]*FI[1]/CN_m + MICtrnN[1] + MICtrnN[4] - DEsorbN #!!! Dividing by CN-m is not in fortran code. Possibly using different N input? ("NlitInput")
+  
+    dLIT_2_N = Inputs[2]*(1-FI[2])/CN_s - LITminN[2] - LITminN[4]
+    dMIC_2_N = upMIC_2_N - (MICtrnN[[4]] + MICtrnN[[5]] + MICtrnN[[6]]) - Nspill[2]
+    dSOM_2_N = Inputs[2]*FI[2]/CN_s + MICtrnN[2] + MICtrnN[5] - OXIDATN
+  
+    dSOM_3_N = MICtrnN[3] + MICtrnN[6] + DEsorbN + OXIDATN - SOMminN[1] - SOMminN[2]
+  
+  #!!! CHECK AGAINST TESTBED CODE, THIS IS WRONG...?
+    # dDIN = (1-NUE[1])*(LITminN[1] + SOMminN[1]) + (1-NUE[2])*(LITminN[2]) +
+    #        (1-NUE[3])*(LITminN[3] + SOMminN[2]) + (1-NUE[4])*(LITminN[4]) +
+    #        Nspill[1] + Nspill[2] - DINup[1] - DINup[2]
+  
+    dDIN = (1-NUE[1])*(LITminN[1] + LITminN[2] + SOMminN[1]) +  #Inputs from r decomp
+           (1-NUE[3])*(LITminN[3] + LITminN[4] + SOMminN[2]) +  #Inputs from K decomp
+           Nspill[1] + Nspill[2] - DINup[1] - DINup[2]    #Uptake to microbial pools and spillage
+    
+    LeachingLoss = Nleak*DIN
+    dDIN = dDIN-LeachingLoss #N leaching losses
+    
+    list(c(dLIT_1, dLIT_2, dMIC_1, dMIC_2, dSOM_1, dSOM_2, dSOM_3, dLIT_1_N, dLIT_2_N, dMIC_1_N, dMIC_2_N, dSOM_1_N, dSOM_2_N, dSOM_3_N, dDIN))
+  })
+}
---
+++ MIMICS-CN_base_ftn.R
@@ -0,0 +1,191 @@
+## Set working drive
+setwd("C:/github/MIMICS_STODE")
+
+#Libraries
+library(rootSolve)
+library(boot)
+library(dplyr)
+library(purrr)
+library(ggplot2)
+library(Metrics)
+
+#bring in RXEQ function
+source("CN_RXEQ.R")
+#source("C:/github/MIMICS_HiRes/MIMICS_ftns/RXEQ_ftn.R")
+
+########################################
+# Set MIMICS default parameters
+########################################
+Vslope  <- rep(0.063, 6)
+Vint    <- rep(5.47, 6)
+aV      <- rep(0.000008, 6)  
+Kslope  <- rep(c(0.025, 0.035, 0.025),2)
+Kint    <- rep(3.19, 6)
+aK      <- rep(10, 6)
+vMOD    <- c(10, 2, 10, 3, 3, 2)
+kMOD    <- c(8, 2, 4, 2, 4, 6)
+KO      <- c(6, 6)
+CUE     <- c(0.55, 0.25, 0.75, 0.35)
+tau_r   <- c(0.00052, 0.3)
+tau_K   <- c(0.00024, 0.1)
+Tau_MOD <- c(100, 0.8, 1.2, 2)
+fPHYS_r <- c(0.3, 1.3)
+fPHYS_K <- c(0.2, 0.8)
+fCHEM_r <- c(0.1, -3, 1)
+fCHEM_K <- c(0.3, -3, 1)
+fSOM_p  <- c(0.000015, -1.5)
+PHYS_scalar <- c(2, -2, NA, NA, NA, NA)
+FI      <- c(0.05, 0.05)
+fmet_p <- c(1, 0.85, 0.013)
+depth <- 30 # set soil depth
+
+#Set default multipliers
+Tau_MULT = 1
+desorb_MULT = 1
+fPHYS_MULT = 1
+
+# N parameters
+NUE <<- rep(0.9, 4) #0.85
+CN_m        <<- 15
+CN_r        <<- 6
+CN_K        <<- 10
+Nleak <<- 0.2 #0.2
+densDep <<- 1 #0.2
+
+###########################################
+# MIMICS single point function
+###########################################
+
+data = read.csv("LTER_SITE_1.csv")
+df <- data[1,] 
+
+Site = df$Site
+ANPP = df$ANPP/2 
+TSOI = df$MAT
+fCLAY = df$CLAY2/100
+lig_N = (df$LIG/100)/(1/(df$CN/2.5))
+fMET <- fmet_p[1] * (fmet_p[2] - fmet_p[3] * lig_N) 
+CN_s        <<- (df$CN-CN_m*fMET)/(1-fMET)
+
+############################################################
+# MIMICS MODEL CODE STARTS HERE
+############################################################
+
+# Calc litter input rate
+EST_LIT <- (ANPP / (365*24)) * 1e3 / 1e4
+#print(EST_LIT)# gC/m2/h (from gC/m2/y) then mgC/cm2/h(from gC/m2/h) 
+
+# ------------ caclulate parameters ---------------
+Vmax     <- exp(TSOI * Vslope + Vint) * aV 
+Km       <- exp(TSOI * Kslope + Kint) * aK
+
+#ANPP strongly correlated with MAP
+Tau_MOD1 <- sqrt(ANPP/Tau_MOD[1])         
+Tau_MOD2 <- Tau_MOD[4]                        
+Tau_MOD1[Tau_MOD1 < Tau_MOD[2]] <- Tau_MOD[2]
+Tau_MOD1[Tau_MOD1 > Tau_MOD[3]] <- Tau_MOD[3] 
+
+tau <- c(tau_r[1]*exp(tau_r[2]*fMET), 
+         tau_K[1]*exp(tau_K[2]*fMET))   
+tau <- tau * Tau_MOD1 * Tau_MOD2 * Tau_MULT 
+
+fPHYS    <- c(fPHYS_r[1] * exp(fPHYS_r[2]*fCLAY), 
+              fPHYS_K[1] * exp(fPHYS_K[2]*fCLAY)) 	            
+fCHEM    <- c(fCHEM_r[1] * exp(fCHEM_r[2]*fMET) * fCHEM_r[3], 
+              fCHEM_K[1] * exp(fCHEM_K[2]*fMET) * fCHEM_K[3]) 	
+fAVAI    <- 1 - (fPHYS + fCHEM)
+
+desorb   <- fSOM_p[1] * exp(fSOM_p[2]*(fCLAY))                  
+
+desorb <- desorb * desorb_MULT
+fPHYS <- fPHYS * fPHYS_MULT
+
+pSCALAR  <- PHYS_scalar[1] * exp(PHYS_scalar[2]*(sqrt(fCLAY)))  #Scalar for texture effects on SOMp
+
+v_MOD    <- vMOD  
+k_MOD    <- kMOD 
+k_MOD[3] <- k_MOD[3] * pSCALAR    
+k_MOD[6] <- k_MOD[6] * pSCALAR    
+
+VMAX     <- Vmax * v_MOD 
+KM       <- Km / k_MOD
+
+#----------initialize pools---------------
+I       <- array(NA, dim=2)             
+I[1]    <- (EST_LIT / depth) * fMET     
+I[2]    <- (EST_LIT / depth) * (1-fMET)
+Inputs <<- I
+lit     <- I   
+mic     <- I  
+som     <- rep(NA, 3) 
+som[1]  <- I[1]
+som[2]  <- I[2]
+som[3]  <- I[1] 
+LITmin  <- rep(NA, dim=4)
+MICtrn  <- c(NA,NA,NA,NA,NA,NA)
+SOMmin  <- rep(NA, dim=2)
+DEsorb  <- rep(NA, dim=1)
+OXIDAT  <- rep(NA, dim=1)
+
+LIT_1_N  <<- .1
+LIT_2_N  <<- .1
+MIC_1_N  <<- .1
+MIC_2_N  <<- .1
+SOM_1_N  <<- .1
+SOM_2_N  <<- .1
+SOM_3_N  <<- .1
+DIN      <<- .1
+
+LITminN   <<- array(NA, dim=4)
+MICtrnN   <<- array(NA, dim=6)
+SOMminN   <<- array(NA, dim=2)
+DEsorbN   <<- array(NA, dim=1)
+OXIDATN   <<- array(NA, dim=1)
+
+DINup     <<- array(NA, dim=2)
+Overflow  <<- array(NA, dim=2)
+Nspill    <<- array(NA, dim=2)
+CNup      <<- array(NA, dim=2)
+upMIC_1   <<-  array(NA, dim=1)
+upMIC_1_N <<-  array(NA, dim=1)
+upMIC_2   <<-  array(NA, dim=1)
+upMIC_2_N <<-  array(NA, dim=1)
+
+
+Tpars <<- c( Inputs = I, VMAX = VMAX, KM = KM, CUE = CUE, 
+             fPHYS = fPHYS, fCHEM = fCHEM, fAVAI = fAVAI, FI = FI, 
+             tau = tau, LITmin = LITmin, SOMmin = SOMmin, MICtrn = MICtrn, 
+             desorb = desorb, DEsorb = DEsorb, OXIDAT = OXIDAT, KO = KO,
+             LITminN = LITminN, SOMminN = SOMminN, MICtrnN = MICtrnN,
+             DEsorbN = DEsorbN, OXIDATN = OXIDATN, densDep=densDep,
+             CNup=CNup, DINup=DINup, Nspill=Nspill, Overflow=Overflow, 
+             upMIC_1=upMIC_1, upMIC_1_N=upMIC_1_N,
+             upMIC_2=upMIC_2, upMIC_2_N=upMIC_2_N,
+             NUE=NUE, CN_m=CN_m, CN_s=CN_s, CN_r=CN_r, CN_K=CN_K, Nleak=Nleak)
+
+Ty    <<- c(LIT_1 = lit[1], LIT_2 = lit[2], 
+            MIC_1 = mic[1], MIC_2 = mic[2], 
+            SOM_1 = som[1], SOM_2 = som[2], SOM_3 = som[3],
+            LIT_1_N = LIT_1_N, LIT_2_N = LIT_2_N, 
+            MIC_1_N = MIC_1_N, MIC_2_N = MIC_2_N, 
+            SOM_1_N = SOM_1_N, SOM_2_N = SOM_2_N, SOM_3_N = SOM_3_N,
+            DIN = DIN)
+
+test  <<- stode(y = Ty, time = 1e6, fun = CN_RXEQ, parms = Tpars, positive = TRUE)
+
+# C only version results:
+# LIT_1       LIT_2       MIC_1       MIC_2       SOM_1       SOM_2       SOM_3 
+# 1.656909890 4.669793042 0.025965813 0.004162176 0.772933300 3.266147911 3.854644406 
+
+### Calc and get MIMICS output 
+MIMLIT    <- (test[[1]][[1]]+test[[1]][[2]])  * depth *1e4 / 1e6 #convert kgC/m2 from mgC/cm3 (0-30 cm) 
+MIMMIC    <- (test[[1]][[3]]+test[[1]][[4]])  * depth *1e4 / 1e6
+MIM_CO    <-  test[[1]][[3]]/test[[1]][[4]]
+MIMSOC    <- sum(test[[1]])  * depth *1e4 / 1e6   
+
+test[[1]]
+attributes(test)
+print(MIMLIT)
+print(MIMMIC)
+print(MIMSOC)
+print(df$SOC)
