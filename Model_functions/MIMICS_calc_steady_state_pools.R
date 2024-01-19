7f646fdc342843623fdef0404d4b54fd02700f0c
Derek Pierson <drk.pierson@gmail.com>
2024-01-19 09:59:11 -0700

    Revised script and folder structure. Next steps: - Paths and code sturcture need to be updated in example scripts. - Constraint script needs to be created. - MC and MCMC scripts need to be added.

---
+++ Model_functions/MIMICS_calc_steady_state_pools.R
@@ -0,0 +1,130 @@
+# Required libraries
+library(rootSolve)
+
+###############################################
+# MIMICS single point function
+#> Input dataframe ("df") must contain columns: SITE, ANPP, fCLAY, TSOI, LIG_N or LIG + CN // Optional: MAT, GWC  
+#>> With output required for running forward
+###############################################
+MIMICS_SS <- function(df){
+  
+  #DEBUG
+  #df = LTER[1,]
+  
+  # Convert all column names to upper case
+  colnames(df) <- toupper(colnames(df))
+  
+  ### Setup a var to collect run notes
+  note <- ""
+  
+  ### Bring in forcing ANPP value, convert gDW to gC
+  ANPP <- df$ANPP/2
+  
+  ### Bring in CLAY value, convert from percent to decimal
+  fCLAY <- df$CLAY/100
+  
+  ### Bring in TSOI value
+  TSOI <- df$TSOI
+  
+  ### Bring in lig:N forcing data
+  LIG_N <- df$LIG_N
+  LIG <- df$LIG
+  CN <- df$CN
+
+  # Bring in soil moisture information
+  theta_liq  <- df$GWC/100  # GWC = Gravimetric water content
+  theta_frzn <- 0           # Not used here. TODO Needs validation. 
+
+  ### Bring in mean annual temperature data
+  MAT <- df$MAT  
+  
+  # Use TSOI if 'MAT' column is not in forcing data
+  if(is.null(MAT)){
+    MAT <- TSOI
+  }
+
+  ############################################################
+  # MIMICS MODEL CODE STARTS HERE
+  ############################################################
+  # function calculates fMET with LIG_N if provided in input data.
+  Tpars <- calc_Tpars_Conly(ANPP=ANPP, fCLAY=fCLAY, TSOI=TSOI, MAT=MAT,     
+                            CN=CN, LIG=LIG, LIG_N=LIG_N,
+                            theta_liq=theta_liq, theta_frzn=theta_frzn) 
+    
+  # Create arrays to hold output
+  lit     <- Tpars$I
+  mic     <- Tpars$I
+  som     <- rep(NA, 3) 
+  som[1]  <- Tpars$I[1]
+  som[2]  <- Tpars$I[2]
+  som[3]  <- Tpars$I[1] 
+  CO2     <- rep(0, 2) 
+
+  Ty    <- c(LIT_1 = lit[1], LIT_2 = lit[2], 
+              MIC_1 = mic[1], MIC_2 = mic[2], 
+              SOM_1 = som[1], SOM_2 = som[2], SOM_3 = som[3])
+  
+  ## Set global parameters to ensure passing of variables to stode function
+  .GlobalEnv$VMAX <- Tpars$VMAX
+  .GlobalEnv$KM <- Tpars$KM
+  .GlobalEnv$fPHYS <- Tpars$fPHYS
+  .GlobalEnv$fCHEM <- Tpars$fCHEM
+  .GlobalEnv$fAVAI <- Tpars$fAVAI
+  .GlobalEnv$I <- Tpars$I
+  .GlobalEnv$tau <- Tpars$tau
+  .GlobalEnv$LITmin <- Tpars$LITmin
+  .GlobalEnv$SOMmin <- Tpars$SOMmin
+  .GlobalEnv$MICtrn <- Tpars$MICtrn
+  .GlobalEnv$desorb <- Tpars$desorb
+  .GlobalEnv$DEsorb <- Tpars$DEsorb
+  .GlobalEnv$OXIDAT <- Tpars$OXIDAT
+  .GlobalEnv$beta <- Tpars$beta
+  
+  # ------------RUN THE MODEL-------------
+  test  <- stode(y = Ty, time = 1e7, fun = RXEQ, parms = Tpars, positive = TRUE)
+  
+  ### Calc and get MIMICS output 
+  MIMLIT    <- (test[[1]][[1]]+test[[1]][[2]]) * depth *1e4 / 1e6 #convert kgC/m2 from mgC/cm3 (0-30 cm) 
+  MIMMIC    <- (test[[1]][[3]]+test[[1]][[4]]) * depth *1e4 / 1e6
+  MIM_CO    <-  test[[1]][[3]]/test[[1]][[4]]
+  MIMSOC    <- sum(test[[1]]) * depth * 1e4 / 1e6   
+  
+  table <- as.numeric(test[[1]])
+  
+  MIMout <- list()
+  MIMout[[1]] <- as.numeric(test[[1]]) * depth *1e4 / 1e6   # MIMICS steady state C pools
+  MIMout[[2]] <- Tpars                                      # MIMICS parameters used for/from simulation
+  MIMout[[3]] <- df                                         # Forcing dataframe
+  MIMout[[4]] <- as.numeric(test[[2]]) * depth *1e4 / 1e6   # CO2-r & CO2-k pools 
+  
+  return(MIMout)
+}
+
+
+#################################################
+# Helper ftn to summarize MIMICS_SS() output
+#################################################
+
+MIMICS_SS_format <- function(MIMICS_SS_output) {
+  
+  MIMout_C_pools <- as.data.frame(t(as.data.frame(MIMICS_SS_output[[1]]))) 
+  colnames(MIMout_C_pools) <- c("LITm", "LITs", "MICr", "MICk", "SOMp", "SOMc", "SOMa")
+ 
+  MIMout_C_pools$MIMSOC <- rowSums(MIMout_C_pools[,1:7])
+  MIMout_C_pools$MIMLIT <- MIMout_C_pools$LITm + MIMout_C_pools$LITs    
+  MIMout_C_pools$MIMMIC <- MIMout_C_pools$MICr + MIMout_C_pools$MICk
+  MIMout_C_pools$MIC_ratio <- MIMout_C_pools$MICr/MIMout_C_pools$MICk
+  
+  MIMout_CO2_pools <- as.data.frame(t(as.data.frame(MIMICS_SS_output[[4]])))
+  colnames(MIMout_CO2_pools) <- c("CO2r", "CO2k")
+  
+  SOMp_desorb_yr <- as.numeric(MIMICS_SS_output[[2]]['desorb']) * 24 * 365
+  MIMout_C_pools$SOMp_Turnover_yrs <- MIMout_C_pools$SOMp/SOMp_desorb_yr
+  
+  MIMout_single_tbl <- cbind(as.data.frame(MIMICS_SS_output[[3]]), MIMout_C_pools %>%
+                               select(MIMSOC, MIMLIT, MIMMIC, MIC_ratio, LITm, LITs, MICr, 
+                                      MICk, SOMa, SOMc, SOMp, SOMp_Turnover_yrs),
+                             MIMout_CO2_pools)
+  return(MIMout_single_tbl)
+}
+
---
+++ Model_functions/MIMICS_sim_litterbag.R
@@ -0,0 +1,134 @@
+
+MIMICS_LITBAG <- function(forcing_df, litBAG, dailyInput=NA, nspin_yrs=10, nspin_days=200, litadd_day=143, verbose=T){
+  
+  #DEBUG
+  #forcing_df <- LTER[6,]
+  #litBAG <- BAGS[1,]
+  #nspin_yrs <- 10
+  #nspin_days <- 200
+  #litadd_day <- 143
+  
+  if(verbose){
+    print("-------------------------------------------------------")
+    print(paste0("Starting ", forcing_df$Site, " - ", litBAG[1]))
+    print("-------------------------------------------------------")
+  }
+  
+  # Get MIMICS steady_state output
+  MIMss <- MIMICS_SS(forcing_df)
+
+  # Create dataframe to store MIMICS pools over timesteps
+  MIMfwd = MIMss[[1]]
+  MIMfwd = (MIMfwd / depth) / (1e4 / 1e6)  #convert from gC m-2 to mgC cm-3
+  
+  # Get Tpars from ss simulation  
+  Tpars = MIMss[[2]]
+
+  #Init arrays to store daily output data
+  nday   <- 365 * nspin_yrs + nspin_days
+  day    <- seq(1,nday,1)
+  year   <- (day-litadd_day)/365
+  doy    <- 1
+    
+  LIT    <- array(NA, dim = c(2,nday))
+  LITBAG <- array(NA, dim = c(2,nday))
+  MIC    <- array(NA, dim = c(2,nday))
+  SOM    <- array(NA, dim = c(3,nday))
+  LITbag_1 = 0. # initial litter bag 
+  LITbag_2 = 0
+
+  sim_year = 0
+  i = 1
+
+  for (d in 1:nday)  { 
+    # For recalc of Tpars from daily forcing data
+    if (!is.na(dailyInput)) {
+      # Set daily Tpars from daily state variables in "dailyInput" dataframe (added in function arguments)
+      Tpars_mod = calc_Tpars( ANPP = dailyInput$ANPP[d], #<-- ENSURE these settings match the ss run
+                             fCLAY = dailyInput$fCLAY[d], 
+                             TSOI = dailyInput$TSOI[d], 
+                             MAT = dailyInput$MAT[d], 
+                             LIG_N = dailyInput$LIG_N[d],
+                             CN = dailyInput$CN[d],  # Only needed if LIG_N not supplied 
+                             LIG = dailyInput$LIG[d],  # Only needed if LIG_N not supplied 
+                             theta_liq = dailyInput$GWC[d], 
+                             theta_frzn = 0) # Change to column name if frozen water content is available 
+    } else {
+      # Use ss Tpars (i.e., same forcing variables for each sim day)
+      Tpars_mod = Tpars
+    }  
+   
+    # Run simulation at hourly timestep
+    for (h in 1:24)   {
+      Ty <- c( LIT_1 = MIMfwd[1], LIT_2 = MIMfwd[2], 
+                 MIC_1 = MIMfwd[3], MIC_2 = MIMfwd[4], 
+                 SOM_1 = MIMfwd[5], SOM_2 = MIMfwd[6], 
+                 SOM_3 = MIMfwd[7])
+      
+      # Run MIMICS simulation step
+      step = RXEQ(t=NA, y=Ty, pars=Tpars_mod)
+      
+      # Litter bag decomp simulation step
+      LITbag  <- rep(NA,4)
+      LITbag[1] <- Ty[[3]] * Tpars_mod$VMAX[1] * LITbag_1 / (Tpars_mod$KM[1] + Ty[[3]])   #MIC_1 mineralization of METABOLIC litter
+      LITbag[2] <- Ty[[3]] * Tpars_mod$VMAX[2] * LITbag_2 / (Tpars_mod$KM[2] + Ty[[3]])   #MIC_1 mineralization of STRUC litter
+      LITbag[3] <- Ty[[4]] * Tpars_mod$VMAX[4] * LITbag_1 / (Tpars_mod$KM[4] + Ty[[4]])   #mineralization of MET litter
+      LITbag[4] <- Ty[[4]] * Tpars_mod$VMAX[5] * LITbag_2 / (Tpars_mod$KM[5] + Ty[[4]])   #mineralization of SRUCTURAL litter
+
+      # Update MIMICS pools
+      #---------------------------------------------
+      MIMfwd = MIMfwd + unlist(step[[1]]) # MIMICS pools
+      
+      LITbag_1 <- LITbag_1 - LITbag[1] - LITbag[3] # Litter bag pools
+      LITbag_2 <- LITbag_2 - LITbag[2] - LITbag[4] 
+      
+
+      # add litter on correct day
+      if (d == litadd_day)   {
+        if (h == 24)  {
+          LITbag_1 <- LITbag_1 + as.numeric(litBAG[3]) # Metabolic pool add
+          LITbag_2 <- LITbag_2 + as.numeric(litBAG[4]) # Structural pool add
+        }
+      }
+          
+      #write out daily results
+      if (h == 24) {
+        LIT[1,d] <- MIMfwd[1]
+        LIT[2,d] <- MIMfwd[2]
+        LITBAG[1,d]  <- LITbag_1
+        LITBAG[2,d]  <- LITbag_2
+        MIC[1,d] <- MIMfwd[3]
+        MIC[2,d] <- MIMfwd[4]
+        SOM[1,d] <- MIMfwd[5]
+        SOM[2,d] <- MIMfwd[6]
+        SOM[3,d] <- MIMfwd[7]
+          
+        #advance day of year counter
+        if (doy == 365) {
+          doy <- 1
+          sim_year = sim_year + 1
+          if(verbose){
+              print(paste0("Finished MIMICS simulation year ", sim_year))
+          }
+        } else {
+          doy <- doy + 1
+        }                         
+      } # close h=24 loop	   						
+    }		#close hour loop
+  }		#close daily loop
+
+  # Compile and format output
+  LITBAG_out <- rbind(as.data.frame(LITBAG), 
+                      as.data.frame(LIT), 
+                      as.data.frame(MIC),
+                      as.data.frame(SOM))
+  
+  LITBAG_out <- LITBAG_out * depth * 1e4 / 1e6 # Soil depth and unit conversion 
+  LITBAG_out <- as.data.frame(t(LITBAG_out))
+  colnames(LITBAG_out) <- c("LITBAGm", "LITBAGs", "LITm", "LITs", "MICr", "MICk", "SOMp", "SOMc", "SOMa")
+  LITBAG_out <- cbind(data.frame(SITE = forcing_df$Site,
+                                 Litter_Type = as.character(litBAG[1]),
+                                 DAY=seq(1:nrow(LITBAG_out))), LITBAG_out)
+  
+  return(LITBAG_out)
+}
